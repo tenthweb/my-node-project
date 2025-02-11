4823c028db6a0cbafd299403f6325c1ed649fd2a
tenthweb <47951574+tenthweb@users.noreply.github.com>
2025-02-11 14:08:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-const express = require('express');
-const app = express();
-
 // Set the port to the value provided by Heroku, or 3000 for local development
 const PORT = process.env.PORT || 3000;
 
